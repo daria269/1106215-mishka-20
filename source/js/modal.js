1f937a8d62f2da66b1cdcd430af577fac2bcf7b1
Daria <dasha_kravchenko_00@mail.ru>
2020-08-21 12:49:11 +0300

    Продолжаем вёрстку личного проекта

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,13 +1,24 @@
 const buyButtons = document.querySelectorAll('.product__button');
 const modalCart = document.querySelector('.modal');
+const buyBtnIndex =  document.querySelector('.popular-item__button');
 
-buyButtons.forEach((butBtn) => {
-  butBtn.addEventListener('click', (e) => {
+buyButtons.forEach((buyBtn) => {
+  buyBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     if (modalCart.classList.contains("hidden")) {
       modalCart.classList.remove("hidden");
     }
   });
 })
+
+if (buyBtnIndex) {
+buyBtnIndex.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (modalCart.classList.contains("hidden")) {
+    modalCart.classList.remove("hidden");
+  }
+  });
+}
 
 modalCart.addEventListener('click', (e) => {
   let modalInner = e.target.closest('.modal__inner');
